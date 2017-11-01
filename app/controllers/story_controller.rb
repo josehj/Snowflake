ce627d4fb3365a2fe6f8c3e16d4fe11ff5ec75bf
Unknown <jhidalgo1@uc.cl>
2017-11-01 15:39:10 -0300

    added views for steps

--- app/controllers/story_controller.rb
+++ app/controllers/story_controller.rb
@@ -12,7 +12,7 @@ class StoryController < ApplicationController
 
   def show
     @story = current_user.stories.find(params[:id])
-    if @story.nil?  
+    if @story.nil?
       redirect_to root_path
     end
   end
